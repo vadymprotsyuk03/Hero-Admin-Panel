bbfdb72aa6c1e67ef5199b003d351c483950fe26
Vadym <vadumprotsyuk@gmail.com>
2024-05-10 21:26:57 +0300

    Store enhancers

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -30,22 +30,14 @@ const HeroesList = () => {
         }
     );
 
-    // const filteredHeroes = useSelector(state => {
-    //     if (state.filters.activeFilter === 'all') {
-    //         console.log('render');
-    //         return state.heroes.heroes;
-    //     } else {
-    //         return state.heroes.heroes.filter(item => item.element === state.activeFilter)
-    //     }
-    // })
 
     const filteredHeroes = useSelector(filteredHeroesSelector);
-    const heroesLoadingStatus = useSelector(state => state.heroesLoadingStatus);
+    const heroesLoadingStatus = useSelector(state => state.heroes.heroesLoadingStatus);
     const dispatch = useDispatch();
     const {request} = useHttp();
 
     useEffect(() => {
-        dispatch(heroesFetching());
+        dispatch('HEROES_FETCHING');
         request("http://localhost:3001/heroes")
             .then(data => dispatch(heroesFetched(data)))
             .catch(() => dispatch(heroesFetchingError()))
